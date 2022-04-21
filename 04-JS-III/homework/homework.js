8aa6d105722f6c4c49a6e72e365e9f1d7083b282
Candela-Altamirano <candelalmd2002@gmail.com>
2022-04-21 01:47:49 -0300

    Homework JS III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -220,9 +220,8 @@ function mayorACien(array) {
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
 var nuevoArray100 = []
-
   for (var i = 0; i < array.length; i++){
-    if (array[i]>100){
+    if (array[i] > 100){
      nuevoArray100.push(array[i])
     }
   }
@@ -238,8 +237,23 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-}
 
+  var nuevoNum = [];
+  for (var i=1; i<11; i++){
+    if(i === numero){
+      break; 
+    }
+    else{
+       nuevoNum.push(numero = numero + 2);
+    }
+}
+if (i<10){
+  return "Se interrumpió la ejecución"; 
+}
+else{
+  return nuevoNum;
+}
+}
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -248,6 +262,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+
+  var nuevoNum = [];
+  for (var i=0; i<10; i++){
+    if(i === 5){
+      continue; 
+    }
+    else{
+       nuevoNum.push(numero = numero + 2);
+    }
+}return nuevoNum; 
 }
 
 
